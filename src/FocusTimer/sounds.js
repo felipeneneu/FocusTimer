7cd948f57642d1fb6ab89223721884c9dc153390
felipeneneu <feliperpg3@gmail.com>
2024-04-12 19:51:56 -0300

    Projeto Timer V1

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/buttonpress.mp3')
+
+export const kitchenTimer = new Audio('./assets/kitchen-timer.mp3')
+
+export const bgAudio = new Audio('./assets/Lost-Frequencies-Are-You-With-Me.mp3')
+
+bgAudio.loop = true
